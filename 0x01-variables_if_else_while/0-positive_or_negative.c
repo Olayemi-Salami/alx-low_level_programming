6f0396c093a1638b2392cd3187bfc7973ea4c84f
Olayemi-Salami <salamimichelleo@gmail.com>
2022-03-12 07:19:00 -0800

    alx is sick

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,8 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
+/* this is the main function */
 int main(void)
 {
 	int n;
@@ -12,9 +14,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
     if(n>0)
+    {
         printf("%i is positive\n", n);
+    }
     else if (n==0)
+    {
         printf("%i is zero\n", n);
+    }
     else
         printf("%i is negative\n", n);
     
